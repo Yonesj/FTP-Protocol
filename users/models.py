cc81ec0df851979d40fc06f7728368b14d4d3035
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-02-06 15:48:29 +0330

    modify user model s fields

--- users/models.py
+++ users/models.py
@@ -4,10 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
-    can_read = models.BooleanField(default=False)
-    can_write = models.BooleanField(default=False)
-    can_delete = models.BooleanField(default=False)
-    can_create = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
