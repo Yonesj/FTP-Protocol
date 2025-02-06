004b41193f7c3822077d6a6d2673868e9823c2ba
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-02-06 15:51:52 +0330

    update user_manager.py based on new user model

--- users/user_manager.py
+++ users/user_manager.py
@@ -1,49 +1,19 @@
 import django
-from django.core.management import call_command
 from users.models import User
-
-
-django.setup()
-call_command("makemigrations", "users")
-call_command("migrate")
 
 
 class UserDBManager:
     @staticmethod
-    def create_user(username, password) -> User | None:
+    def create_user(username, password, is_admin=False) -> User | None:
         if User.objects.filter(username=username).exists():
             return None  # User already exists
 
         user = User(
             username=username,
             password=password,
-            can_read=True,
-            can_write=False,
-            can_delete=False,
-            can_create=True,
+            is_admin=is_admin
         )
         user.save()
-        return user
-
-    @staticmethod
-    def create_admin(username, password) -> User | None:
-        """
-        Creates an admin user with all permissions enabled.
-        This method can only be called via command line.
-        """
-        if User.objects.filter(username=username).exists():
-            return None  # User already exists
-
-        user = User(
-            username=username,
-            password=password,
-            can_read=True,
-            can_write=True,
-            can_delete=True,
-            can_create=True,
-        )
-        user.save()
-        print(f"Admin user '{username}' created successfully.")
         return user
 
     @staticmethod
@@ -54,4 +24,5 @@ class UserDBManager:
             return None
 
 
+django.setup()
 USER_DB_MANAGER = UserDBManager()
